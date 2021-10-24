67ed3a87b7f0d1e85acd6cf8d69991cae76ab64a
Nicolò Sordoni <nicolo.sordoni@antreem.com>
2021-10-24 14:05:23 +0200

    Login page layout

--- lib/login/login_page.dart
+++ lib/login/login_page.dart
@@ -1,10 +1,56 @@
+import 'package:droidconit_21_flutter_navigation_demo/common/routing/app_pages.dart';
+import 'package:droidconit_21_flutter_navigation_demo/common/routing/bloc/routing_bloc.dart';
+import 'package:droidconit_21_flutter_navigation_demo/common/routing/bloc/routing_event.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+
+import 'package:provider/provider.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text('Login Page');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Page'),
+      ),
+      body: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'username',
+              ),
+              TextField(
+                controller: TextEditingController(text: 'nicol_ograve'),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                'password',
+              ),
+              TextField(
+                obscureText: true,
+                controller: TextEditingController(text: 'Password1!'),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Center(
+                  child: MaterialButton(
+                onPressed: () {
+                  context
+                      .read<RoutingBloc>()
+                      .add(RoutePushed(newPage: TodosListPageConfig));
+                },
+                child: Text('Login'),
+                color: Colors.green,
+              ))
+            ],
+          )),
+    );
   }
 }
